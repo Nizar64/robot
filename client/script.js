bd6099f50e7ee56d951c524e13d0128619d8e296
Nizar64 <46851791+Nizar64@users.noreply.github.com>
2023-02-11 18:15:42 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://127.0.0.1:5173', {
+    const response = await fetch('https://robot-gqf6.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
